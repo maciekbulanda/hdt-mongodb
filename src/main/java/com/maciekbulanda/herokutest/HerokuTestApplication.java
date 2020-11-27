c6101825e1845b354c6c9d03922d7a199fe381aa
maciekbulanda <bma@post.pl>
2020-11-27 14:31:35 +0100

    Initial commit

---
+++ src/main/java/com/maciekbulanda/herokutest/HerokuTestApplication.java
@@ -0,0 +1,13 @@
+package com.maciekbulanda.herokutest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maciekbulanda/herokutest/HerokuTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maciekbulanda.herokutest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
