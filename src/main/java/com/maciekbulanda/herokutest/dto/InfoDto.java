8ccdca6da8708fe249ee21d5e73b9f54649326ee
maciekbulanda <bma@post.pl>
2020-11-27 15:49:04 +0100

    info updated

--- src/main/java/com/maciekbulanda/herokutest/dto/InfoDto.java
+++ src/main/java/com/maciekbulanda/herokutest/dto/InfoDto.java
@@ -1,9 +1,13 @@
 package com.maciekbulanda.herokutest.dto;
 
 public class InfoDto {
+    private String name;
     private String version;
+    private String creator;
     public InfoDto() {
+        this.name = "Heroku With MongoDB Test App";
         this.version = "1";
+        this.creator = "Maciej Bulanda";
     }
 
     public String getVersion() {
@@ -12,5 +16,21 @@ public class InfoDto {
 
     public void setVersion(String version) {
         this.version = version;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
     }
 }
