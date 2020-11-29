baf5415ac5f9f1b83114a5d362712a8871b1d856
maciek <bma@post.pl>
2020-11-29 11:49:05 +0100

    CRUD functions to MainController

--- src/main/java/com/maciekbulanda/herokutest/controller/MainController.java
+++ src/main/java/com/maciekbulanda/herokutest/controller/MainController.java
@@ -3,12 +3,11 @@ package com.maciekbulanda.herokutest.controller;
 import com.maciekbulanda.herokutest.docs.TempReading;
 import com.maciekbulanda.herokutest.dto.InfoDto;
 import com.maciekbulanda.herokutest.repository.TempReadingRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@CrossOrigin(origins = {"http://localhost:3000", "https://hdt-react.herokuapp.com"})
 @RestController
 public class MainController {
 
@@ -23,9 +22,19 @@ public class MainController {
         return Mono.just(new InfoDto());
     }
 
+    @GetMapping("/api/tempreading")
+    Flux<TempReading> getReadings() {
+        return tempReadingRepository.findAll();
+    }
+
     @PostMapping("/api/tempreading")
-    Mono<TempReading> add(@RequestBody TempReading reading) {
+    Mono<TempReading> addReading(@RequestBody TempReading reading) {
         return tempReadingRepository.save(reading);
+    }
+
+    @DeleteMapping("/api/tempreading")
+    Mono<Void> deleteReading(@RequestParam String id) {
+        return tempReadingRepository.deleteById(id);
     }
 
 }
