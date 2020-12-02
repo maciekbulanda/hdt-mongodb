4eddbae668b1b669ae28de4651f6da1fc30842a8
maciekbulanda <bma@post.pl>
2020-12-02 16:17:11 +0100

    added UserDetailsService

--- src/main/java/com/maciekbulanda/herokutest/config/SecurityConfig.java
+++ src/main/java/com/maciekbulanda/herokutest/config/SecurityConfig.java
@@ -1,4 +1,26 @@
 package com.maciekbulanda.herokutest.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        var userDetailsService = new InMemoryUserDetailsManager();
+
+        var user = User
+                .withUsername("user")
+                .password("user")
+                .authorities("user")
+                .build();
+
+        userDetailsService.createUser(user);
+        return userDetailsService;
+    }
 }
